e75368a808ecd469b4142282ab3db7ed654325d0
RoryChang95 <rory@ispan.com.tw>
2023-01-05 10:35:05 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MSIT45Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
